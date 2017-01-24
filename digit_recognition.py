50e19e2b733e4872ff9cab90dff35450bc6ca228
HariKolakaleti <hari.kolakaleti@gmail.com>
2017-01-24 01:31:50 -0800

    comment out mnist for now

--- digit_recognition.py
+++ digit_recognition.py
@@ -41,8 +41,8 @@ from IPython.display import display
 #%%
 
 # mnist dataset
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True) 
+#from tensorflow.examples.tutorials.mnist import input_data
+#mnist = input_data.read_data_sets('MNIST_data', one_hot=True) 
 
 #%%
 
